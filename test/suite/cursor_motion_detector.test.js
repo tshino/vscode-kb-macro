68cfcb9f6f39c16219a0812f6c25ad4121963c8b
tshino <tshino.ssr@gmail.com>
2021-10-08 00:50:39 +0900

    Add tests for CursorMotionDetector

--- test/suite/cursor_motion_detector.test.js
+++ test/suite/cursor_motion_detector.test.js
@@ -17,48 +17,72 @@ describe('CursorMotionDetector', () => {
             assert.strictEqual(cursorMotionDetector.getExpectedSelections(), null);
         });
     });
-    describe('implicit motion', () => {
-        it('should detect the difference between the actual position and predicted one (move to left)', async () => {
-            const logs = [];
-            const cursorMotionDetector = CursorMotionDetector();
-            cursorMotionDetector.onDetectCursorMotion(info => {
-                logs.push(info);
-            });
-            const textEditor = {
-                selections: [ new vscode.Selection(3, 4, 3, 4) ]
-            };
-            cursorMotionDetector.start(textEditor);
-            const predicted = [ new vscode.Selection(3, 7, 3, 7) ];
-            cursorMotionDetector.setExpectedSelections(predicted);
-            const event = {
-                textEditor: textEditor,
-                selections: [ new vscode.Selection(3, 6, 3, 6) ]
-            };
-            cursorMotionDetector.processSelectionChangeEvent(event);
-            cursorMotionDetector.stop();
-
-            assert.deepStrictEqual(logs, [ MoveLeft(1) ]);
+    const testDetection = function({ init, inputs, expectedLogs }) {
+        const logs = [];
+        const cursorMotionDetector = CursorMotionDetector();
+        cursorMotionDetector.onDetectCursorMotion(info => {
+            logs.push(info);
         });
-        it('should detect implicit motion (move to right)', async () => {
-            const logs = [];
-            const cursorMotionDetector = CursorMotionDetector();
-            cursorMotionDetector.onDetectCursorMotion(info => {
-                logs.push(info);
-            });
-            const textEditor = {
-                selections: [ new vscode.Selection(3, 4, 3, 4) ]
-            };
-            cursorMotionDetector.start(textEditor);
-            const predicted = [ new vscode.Selection(3, 7, 3, 7) ];
-            cursorMotionDetector.setExpectedSelections(predicted);
-            const event = {
-                textEditor: textEditor,
-                selections: [ new vscode.Selection(3, 8, 3, 8) ]
-            };
-            cursorMotionDetector.processSelectionChangeEvent(event);
-            cursorMotionDetector.stop();
+        const textEditor = {
+            selections: init
+        };
+        cursorMotionDetector.start(textEditor);
+        for (let i = 0; i < inputs.length; i++) {
+            const input = inputs[i];
+            if ('predicted' in input) {
+                cursorMotionDetector.setExpectedSelections(input.predicted);
+            } else if ('changed' in input) {
+                const event = {
+                    textEditor: textEditor,
+                    selections: input.changed
+                };
+                cursorMotionDetector.processSelectionChangeEvent(event);
+            }
+        }
+        cursorMotionDetector.stop();
 
-            assert.deepStrictEqual(logs, [ MoveRight(1) ]);
+        assert.deepStrictEqual(logs, expectedLogs);
+    };
+    describe('implicit motion with prediction', () => {
+        it('should detect the difference between the actual position and predicted one (move to left)', async () => {
+            testDetection({
+                init: [ new vscode.Selection(3, 4, 3, 4) ],
+                inputs: [
+                    { predicted: [ new vscode.Selection(3, 7, 3, 7) ] },
+                    { changed: [ new vscode.Selection(3, 6, 3, 6) ] }
+                ],
+                expectedLogs: [ MoveLeft(1) ]
+            });
+        });
+        it('should detect implicit motion with prediction (move to right)', async () => {
+            testDetection({
+                init: [ new vscode.Selection(3, 4, 3, 4) ],
+                inputs: [
+                    { predicted: [ new vscode.Selection(3, 7, 3, 7) ] },
+                    { changed: [ new vscode.Selection(3, 8, 3, 8) ] }
+                ],
+                expectedLogs: [ MoveRight(1) ]
+            });
+        });
+    });
+    describe('implicit motion without prediction', () => {
+        it('should detect the unexpected motion of cursor (move to left)', async () => {
+            testDetection({
+                init: [ new vscode.Selection(3, 6, 3, 6) ],
+                inputs: [
+                    { changed: [ new vscode.Selection(3, 5, 3, 5) ] }
+                ],
+                expectedLogs: [ MoveLeft(1) ]
+            });
+        });
+        it('should detect the unexpected motion of cursor (move to right)', async () => {
+            testDetection({
+                init: [ new vscode.Selection(3, 6, 3, 6) ],
+                inputs: [
+                    { changed: [ new vscode.Selection(3, 7, 3, 7) ] }
+                ],
+                expectedLogs: [ MoveRight(1) ]
+            });
         });
     });
     // TODO: add more tests for CursorMotionDetector
