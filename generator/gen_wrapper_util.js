68184fbd133ecee183a5207cba226de8e38cd3d0
tshino <tshino.ssr@gmail.com>
2021-11-10 23:12:58 +0900

    Small refactoring

--- generator/gen_wrapper_util.js
+++ generator/gen_wrapper_util.js
@@ -142,48 +142,56 @@ function makeWhenSubsetContext(contextList, bitmap) {
     }
 }
 
+function makeCombinedKeybindingsForKey(contextList, keyDict, commonKeyDict, key) {
+    let combined = [];
+    const commonKeybindings = commonKeyDict.get(key) || [];
+    const dict = keyDict.get(key);
+
+    // Reorder and unify keybindings.
+    const indices = Array(contextList.length).fill(0);
+    for (let i = 0; i <= commonKeybindings.length; i++) {
+        for (let j = 0; j < contextList.length; j++) {
+            const pos = (
+                i < commonKeybindings.length
+                    ? commonKeybindings[i].positions[j]
+                    : (dict.get(contextList[j]) || []).length
+            );
+            if (pos < 0) {
+                continue;
+            }
+            const context = contextList[j];
+            const keybindings = dict.get(context) || [];
+            const sliced = keybindings.slice(indices[j], pos);
+            const converted = addKeybindingsContext(sliced, context);
+            combined = combined.concat(converted);
+            indices[j] = pos + 1;
+        }
+        if (i < commonKeybindings.length) {
+            // unified keybinding
+            const keybinding = commonKeybindings[i].keybinding;
+            const bitmap = commonKeybindings[i].positions.map(pos => 0 <= pos);
+            if (!bitmap.every(enable => enable)) {
+                // partial common keybindings (e.g. 'isWindows || isLinux')
+                const subset = makeWhenSubsetContext(contextList, bitmap);
+                keybinding.when = addWhenContext(keybinding.when, subset);
+            }
+            combined.push(keybinding);
+        }
+    }
+    return combined;
+}
+
 // Make combined keybindings of different default keybindings of windows, macos and linux.
 function combineBaseKeybingings(baseKeybindings) {
     const contextList = baseKeybindings.map(item => item.context);
     const keyDict = makeKeyDict(baseKeybindings);
     const commonKeyDict = makeCommonKeybindingsDict(contextList, keyDict);
-    let combined = [];
-    for (const [ key, dict ] of keyDict) {
-        const commonKeybindings = commonKeyDict.get(key) || [];
-
-        // Reorder and unify keybindings.
-        const indices = Array(contextList.length).fill(0);
-        for (let i = 0; i <= commonKeybindings.length; i++) {
-            for (let j = 0; j < contextList.length; j++) {
-                const pos = (
-                    i < commonKeybindings.length
-                        ? commonKeybindings[i].positions[j]
-                        : (dict.get(contextList[j]) || []).length
-                );
-                if (pos < 0) {
-                    continue;
-                }
-                const context = contextList[j];
-                const keybindings = dict.get(context) || [];
-                const sliced = keybindings.slice(indices[j], pos);
-                const converted = addKeybindingsContext(sliced, context);
-                combined = combined.concat(converted);
-                indices[j] = pos + 1;
-            }
-            if (i < commonKeybindings.length) {
-                // unified keybinding
-                const keybinding = commonKeybindings[i].keybinding;
-                const bitmap = commonKeybindings[i].positions.map(pos => 0 <= pos);
-                if (!bitmap.every(enable => enable)) {
-                    // partial common keybindings (e.g. 'isWindows || isLinux')
-                    const subset = makeWhenSubsetContext(contextList, bitmap);
-                    keybinding.when = addWhenContext(keybinding.when, subset);
-                }
-                combined.push(keybinding);
-            }
-        }
+    let keybindings = [];
+    for (const key of keyDict.keys()) {
+        const combined = makeCombinedKeybindingsForKey(contextList, keyDict, commonKeyDict, key);
+        keybindings = keybindings.concat(combined);
     }
-    return combined;
+    return keybindings;
 }
 
 module.exports = {
