d73b0a9169c85e130590d11029bbe376481923ba
tshino <tshino.ssr@gmail.com>
2021-11-24 23:52:16 +0900

    Add tests for util.js

--- test/suite/util.test.js
+++ test/suite/util.test.js
@@ -5,6 +5,7 @@ const util = require('../../src/util.js');
 
 describe('util', () => {
     const Selection = (l1,c1,l2,c2) => new vscode.Selection(l1,c1,l2,c2);
+    const Range = (l1,c1,l2,c2) => new vscode.Range(l1,c1,l2,c2);
 
     describe('isEqualSelections', () => {
         it('should return true if two selections are equal (1)', async () => {
@@ -86,6 +87,71 @@ describe('util', () => {
             ];
             const result = util.makeIndexOfSortedSelections(input);
             assert.deepStrictEqual(result, [0, 3, 1, 2]);
+        });
+    });
+    describe('makeSelectionsAfterTyping', () => {
+        it('should calculate predicted locations where cursors should move to after typing', () => {
+            const changes = [ { range: Range(3, 0, 3, 0), text: 'a' } ];
+            const expected = [ Selection(3, 1, 3, 1) ];
+            assert.deepStrictEqual(util.makeSelectionsAfterTyping(changes), expected);
+        });
+        it('should make prediction (multiple characters)', () => {
+            const changes = [ { range: Range(3, 0, 3, 0), text: 'abcde' } ];
+            const expected = [ Selection(3, 5, 3, 5) ];
+            assert.deepStrictEqual(util.makeSelectionsAfterTyping(changes), expected);
+        });
+        it('should make prediction (multi-cursor)', () => {
+            const changes = [
+                { range: Range(3, 1, 3, 1), text: 'a' },
+                { range: Range(4, 1, 4, 1), text: 'a' }
+            ];
+            const expected = [ Selection(3, 2, 3, 2), Selection(4, 2, 4, 2) ];
+            assert.deepStrictEqual(util.makeSelectionsAfterTyping(changes), expected);
+        });
+        it('should make prediction (multi-cursor in the same line)', () => {
+            const changes = [
+                { range: Range(3, 1, 3, 1), text: 'ab' },
+                { range: Range(3, 5, 3, 5), text: 'ab' }
+            ];
+            const expected = [ Selection(3, 3, 3, 3), Selection(3, 9, 3, 9) ];
+            assert.deepStrictEqual(util.makeSelectionsAfterTyping(changes), expected);
+        });
+        it('should make prediction (typing with a selection that contains line-breaks)', () => {
+            const changes = [ { range: Range(12, 1, 14, 5), text: 'x' } ];
+            const expected = [ Selection(12, 2, 12, 2) ];
+            assert.deepStrictEqual(util.makeSelectionsAfterTyping(changes), expected);
+        });
+        it('should make prediction (typing with multiple selections)', () => {
+            const changes = [
+                { range: Range(12, 1, 12, 5), text: 'x' },
+                { range: Range(13, 1, 13, 5), text: 'x' }
+            ];
+            const expected = [ Selection(12, 2, 12, 2), Selection(13, 2, 13, 2) ];
+            assert.deepStrictEqual(util.makeSelectionsAfterTyping(changes), expected);
+        });
+        it('should make prediction (typing with multiple selections that contains line-breaks) (1)', () => {
+            const changes = [
+                { range: Range(12, 1, 14, 5), text: 'x' },
+                { range: Range(15, 1, 16, 5), text: 'x' }
+            ];
+            const expected = [ Selection(12, 2, 12, 2), Selection(13, 2, 13, 2) ];
+            assert.deepStrictEqual(util.makeSelectionsAfterTyping(changes), expected);
+        });
+        it('should make prediction (typing with multiple selections that contains line-breaks) (2)', () => {
+            const changes = [
+                { range: Range(12, 1, 14, 5), text: 'x' },
+                { range: Range(14, 7, 16, 5), text: 'x' }
+            ];
+            const expected = [ Selection(12, 2, 12, 2), Selection(12, 5, 12, 5) ];
+            assert.deepStrictEqual(util.makeSelectionsAfterTyping(changes), expected);
+        });
+        it('should make prediction (typing with multiple selections in a single line)', () => {
+            const changes = [
+                { range: Range(12, 1, 12, 3), text: 'x' },
+                { range: Range(12, 6, 12, 8), text: 'x' }
+            ];
+            const expected = [ Selection(12, 2, 12, 2), Selection(12, 6, 12, 6) ];
+            assert.deepStrictEqual(util.makeSelectionsAfterTyping(changes), expected);
         });
     });
 });
