d7f37d23566c49e6854f8a5ddddc6dd39e3d6453
tshino <tshino.ssr@gmail.com>
2021-11-30 19:30:14 +0900

    Use extension-id on file name

--- keymap-wrapper/update.sh
+++ keymap-wrapper/update.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir -p keymap-wrapper/tmp
 
-KEYMAP_ID=vscode-vz-like-keymap
+KEYMAP_ID=tshino.vz-like-keymap
 GITHUB_REPO_URL=`cat keymap-wrapper/${KEYMAP_ID}.config.json | json base.githubRepoURL`
 GITHUB_TAG=`basename \`curl -ILSs -o /dev/null -w '%{url_effective}\n' ${GITHUB_REPO_URL}/releases/latest\``
 PACKAGE_JSON_URL=${GITHUB_REPO_URL}/raw/${GITHUB_TAG}/package.json
