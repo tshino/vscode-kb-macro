162ec6d1b93d7da87ea03427b2252fe776f234be
tshino <tshino.ssr@gmail.com>
2021-12-01 00:50:55 +0900

    Update keymap wrapper generator script #11

--- keymap-wrapper/update.sh
+++ keymap-wrapper/update.sh
@@ -3,11 +3,13 @@ set -e
 
 mkdir -p keymap-wrapper/tmp
 
-KEYMAP_ID=tshino.vz-like-keymap
-GITHUB_REPO_URL=`cat keymap-wrapper/${KEYMAP_ID}.config.json | json base.githubRepoURL`
-GITHUB_TAG=`basename \`curl -ILSs -o /dev/null -w '%{url_effective}\n' ${GITHUB_REPO_URL}/releases/latest\``
-PACKAGE_JSON_URL=${GITHUB_REPO_URL}/raw/${GITHUB_TAG}/package.json
-echo '** downloading '${PACKAGE_JSON_URL}
-curl -L -o keymap-wrapper/tmp/${KEYMAP_ID}.package.json ${PACKAGE_JSON_URL}
+for CONFIG in keymap-wrapper/*.config.json; do
+    KEYMAP_ID=`basename $CONFIG .config.json`
+    GITHUB_REPO_URL=`json -f $CONFIG base.githubRepoURL`
+    GITHUB_TAG=`basename \`curl -ILSs -o /dev/null -w '%{url_effective}\n' ${GITHUB_REPO_URL}/releases/latest\``
+    PACKAGE_JSON_URL=${GITHUB_REPO_URL}/raw/${GITHUB_TAG}/package.json
+    echo '** downloading '${PACKAGE_JSON_URL}
+    curl -L -o keymap-wrapper/tmp/${KEYMAP_ID}.package.json ${PACKAGE_JSON_URL}
+done
 
 node generator/gen_keymap_wrapper.js
