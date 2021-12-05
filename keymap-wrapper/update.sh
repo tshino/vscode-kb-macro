e2a2edb78e2d1aaac53c75ec4dad36f1eaccc676
tshino <tshino.ssr@gmail.com>
2021-12-05 14:53:49 +0900

    Update update.sh to enable general URL for package.json #11

--- keymap-wrapper/update.sh
+++ keymap-wrapper/update.sh
@@ -5,10 +5,18 @@ mkdir -p keymap-wrapper/tmp
 
 for CONFIG in keymap-wrapper/*.config.json; do
     KEYMAP_ID=`basename $CONFIG .config.json`
-    GITHUB_REPO_URL=`json -f $CONFIG base.githubRepoURL`
-    GITHUB_TAG=`basename \`curl -ILSs -o /dev/null -w '%{url_effective}\n' ${GITHUB_REPO_URL}/releases/latest\``
-    PACKAGE_JSON_URL=${GITHUB_REPO_URL}/raw/${GITHUB_TAG}/package.json
-    echo '** downloading '${PACKAGE_JSON_URL}
+    TYPE=`json -f $CONFIG base.type`
+    case "$TYPE" in
+        "github-latest-release" )
+            REPO_URL=`json -f $CONFIG base.repo`
+            LATEST_TAG=`basename \`curl -ILSs -o /dev/null -w '%{url_effective}\n' ${REPO_URL}/releases/latest\``
+            PACKAGE_JSON_URL=${REPO_URL}/raw/${LATEST_TAG}/package.json
+            ;;
+        "url" )
+            PACKAGE_JSON_URL=`json -f $CONFIG base.url`
+            ;;
+    esac
+    echo "** downloading $PACKAGE_JSON_URL"
     curl -L -o keymap-wrapper/tmp/${KEYMAP_ID}.package.json ${PACKAGE_JSON_URL}
 done
 
