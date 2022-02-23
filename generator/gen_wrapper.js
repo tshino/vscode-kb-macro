ac37d4d2dbe224876ca2b2f2ca3a77dc3c6c04dc
tshino <tshino.ssr@gmail.com>
2022-02-24 00:56:09 +0900

    Set exit code on failure

--- generator/gen_wrapper.js
+++ generator/gen_wrapper.js
@@ -65,4 +65,7 @@ async function main() {
     await genWrapperUtil.writeJSON(PackageJsonPath, packageJson);
 }
 
-main();
+main().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
