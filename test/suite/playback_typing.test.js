dbccd0a3aee38c74ec0380a98e5120e4f3506a99
tshino <tshino.ssr@gmail.com>
2022-12-31 17:38:04 +0900

    Add tests for background recording of typing

--- test/suite/playback_typing.test.js
+++ test/suite/playback_typing.test.js
@@ -712,7 +712,7 @@ describe('Recording and Playback: Typing', () => {
         });
     });
 
-    describe('background recording of direct typing', () => {
+    describe('background recording of typing', () => {
         beforeEach(async () => {
             await TestUtil.resetDocument(textEditor, (
                 '\n'.repeat(10) +
@@ -728,6 +728,15 @@ describe('Recording and Playback: Typing', () => {
             assert.deepStrictEqual(keyboardMacro.getHistory(), [ Type('X') ]);
             assert.strictEqual(textEditor.document.lineAt(0).text, 'X');
             assert.deepStrictEqual(getSelections(), [[0, 1]]);
+        });
+        it('should record typing of an opening bracket which triggers bracket completion', async () => {
+            await setSelections([[10, 4]]);
+            await keyboardMacro.enableBackgroundRecording();
+            await vscode.commands.executeCommand('type', { text: '(' });
+            await keyboardMacro.disableBackgroundRecording();
+            assert.deepStrictEqual(keyboardMacro.getHistory(), [ Type('()'), MoveLeft(1) ]);
+            assert.strictEqual(textEditor.document.lineAt(10).text, 'abcd()');
+            assert.deepStrictEqual(getSelections(), [[10, 5]]);
         });
     });
 });
